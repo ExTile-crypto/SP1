a794160306aba30a13e45db973cd6d4daf99b7e3
Cezary <cezaryr54@gmail.com>
2026-01-28 20:23:29 +0100

    v0.1

---
+++ src/main1.java
@@ -0,0 +1,49 @@
+public class main1 {
+    void main(){
+String empty=("");
+
+
+String bandName=("Ostehapserne");
+    //Band profile
+int fans=(42069);
+int maxFans=(50000);
+int fameLevel=(3);
+int exp=(9600);
+double money=(7800.25);
+boolean isActive=(true);
+String genre=("PU (Polish Underground)");
+
+
+String[] songs={"Song1","Song2","Song3","Song4"};
+
+        System.out.println("¤-¤-¤ BAND PROFILE ¤-¤-¤");
+        System.out.println("Band name: "+bandName);
+        System.out.println("Genre: "+genre);
+        System.out.println("Fame level: "+fameLevel);
+        System.out.println("Fans: "+fans+"/"+maxFans);
+        System.out.println("XP: "+exp);
+        System.out.println("Money: " +money+"PLN");
+        System.out.println("Active: "+isActive);
+
+        System.out.println(empty);
+        System.out.println("# of tracks: "+songs.length);
+        System.out.println("Track List:");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(" - "+songs[i]);
+        }
+
+
+
+            //int count = 0;
+            //int i = 1;
+
+            //while (i<4)){
+                //if (i%2==0) {
+                  //  count++;
+                //}
+
+        }
+    }
+
+
+
